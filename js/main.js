7e6dce955cc6b327fc0299f67574986cecf47368
souurabhgavz <souurabhgavhane@gmail.com>
2021-08-26 09:56:10 +0530

    like button added on blogs page

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@
 
 	// bootstrap dropdown hover
 
+  //blog page like
+  $(function(){
+    $(document).one('click', '.like-review', function(e) {
+      $(this).html('<i class="fa fa-heart" aria-hidden="true"></i> You liked this');
+      $(this).children('.fa-heart').addClass('animate-like');
+    });
+  });
+
   // loader
   var loader = function() {
     setTimeout(function() { 
