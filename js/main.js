8197b0e6dc03592bc07e5845b28e7c3a9e07ed15
Shraavya <shraavya71rao@gmail.com>
2021-09-01 12:34:48 +0530

    search bar functionality

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@
 	'use strict';
 
 	// bootstrap dropdown hover
+// search
+
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".dropdown-menu li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+
+
 
   //blog page like
   $(function(){
